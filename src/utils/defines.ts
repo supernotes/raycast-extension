874de5073e1a6f8453db6aec9d58ed714b44a0ff
tobeagram <tobiaswhetton@hotmail.com>
2022-11-08 09:32:33 +0000

    Add support for opening in Supernotes

--- src/utils/defines.ts
+++ src/utils/defines.ts
@@ -3,3 +3,4 @@ import { environment } from "@raycast/api";
 export const SUPERNOTES_API_URL = environment.isDevelopment
   ? "http://127.0.0.1:5000/v1"
   : "https://api.supernotes.app/v1";
+export const SUPERNOTES_APP_LINK_URL = 'supernotes://'
